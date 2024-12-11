b65ebfd597f9a3e066f69ae2c82eeaa1a668363c
Suryansh Shakya <Suryansh.Shakya@scaler.com>
2024-12-11 15:50:36 +0530

    fix: type for fully connected layer

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ class LSTMModel(nn.Module):
         super(LSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.lc = nn.Linear(hidden_size, output_size)
+        self.fc = nn.Linear(hidden_size, output_size)
     
     def forward(self, x):
         out, _ = self.lstm(x)
