431df2dd7f065c60c552b9778cb6b282ce41181d
Suryansh Shakya <Suryansh.Shakya@scaler.com>
2024-12-11 15:17:31 +0530

    add: modle.py

---
+++ model.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lc = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])
+        return out
