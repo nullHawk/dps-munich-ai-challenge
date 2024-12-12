ad8d3f650eadb6bc5e7f1a9d36cf71a78b9ea27a
nullHawk <Suryansh.Shakya@scaler.com>
2024-12-12 14:48:04 +0530

    done with LSTM

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=3):
         super(LSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
